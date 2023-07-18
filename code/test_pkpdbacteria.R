916f88f749aaa250da2e8eed2cd5d21091c9e577
Andreas Handel <andreas.handel@gmail.com>
2023-07-18 16:45:27 -0700

    updated code

--- code/test_pkpdbacteria.R
+++ code/test_pkpdbacteria.R
@@ -1,9 +1,22 @@
 library(DSAIRM)
 source("simulate_pkpdbacteria_ode.R")
 
-sim <- simulate_pkpdbacteria_ode(B = 100, I = 1, g = 1, Bmax = 1e+05, 
-                            dB = 0.5, kI = 1e-4, r = 1e-4, dI = 2,
-                            C0 = 1, dC = 1, C50 = 1, k = 1, Emax = 0,
-                            txstart = 10, txinterval = 1, tstart = 0, tfinal = 200, dt = 0.01)
+# running new bacteria model with drug PK/PD to explore impact of drug
+# simulation 1: drug present but not impact (Emax=0)
+sim1 <- simulate_pkpdbacteria_ode(B = 10, I = 1, g = 1, Bmax = 1e+03, 
+                            dB = 0.1, kI = 1e-3, r = 2e-3, dI = 1,
+                            C0 = 1000, dC = 0.1, C50 = 500, k = 1, Emax = 0,
+                            txstart = 10, txinterval = 10, tstart = 0, 
+                            tfinal = 200, dt = 0.01)
+sim1$yscale = "identity" #set to "log10" for log plot
+generate_ggplot(list(sim1))
 
-generate_ggplot(list(sim))
+
+# simulation 1: drug present and working (Emax>0)
+sim2 <- simulate_pkpdbacteria_ode(B = 10, I = 1, g = 1, Bmax = 1e+03, 
+                                 dB = 0.1, kI = 1e-3, r = 2e-3, dI = 1,
+                                 C0 = 1000, dC = 0.1, C50 = 500, k = 1, Emax = 1,
+                                 txstart = 10, txinterval = 10, tstart = 0, 
+                                 tfinal = 200, dt = 0.01)
+sim2$yscale = "identity"
+generate_ggplot(list(sim2))
